57c049626c7bc75d6557162872d6fe08a95a1883
maxkusnadi <max@theteamie.com>
2016-07-21 20:30:20 +0800

    decorator done, functions defined

--- app/constants.py
+++ app/constants.py
@@ -1,7 +1,19 @@
-CREATE_KEYWORDS = ("create", "make")
-DELETE_KEYWORDS = ("delete", "remove")
-UPDATE_KEYWORDS = ("update", "add")
-QUERY_KEYWORDS = ("find", "search", "list")
-CLASSROOM = ("classroom", "class")
+CREATE_KEYWORDS = ("create", "make", 'add')
+DELETE_KEYWORDS = ("delete", "remove", 'erase')
+UPDATE_KEYWORDS = ("update", "edit")
+QUERY_KEYWORDS = ("find", "search", "list", 'show')
+CLASSROOM = ("classroom", "class", 'lesson')
+ASSIGNMENT = ('assignment', 'submission', 'test', 'exam', 'homework')
 
 WORD2VEC_MODEL_PATH = "static/word2vec_model.bin"
+
+# Response
+CREATE_CLASSROOM = "Classroom created"
+DELETE_CLASSROOM = "Classroom deleted"
+UPDATE_CLASSROOM = "Classroom updated"
+QUERY_CLASSROOM = "Classroom search"
+
+CREATE_ASSIGNMENT = "Assignment created"
+DELETE_ASSIGNMENT = "Assignment deleted"
+UPDATE_ASSIGNMENT = "Assignment updated"
+QUERY_ASSIGNMENT = "Assignment search"
---
+++ app/decorator.py
@@ -0,0 +1,6 @@
+from app.type import *
+
+create = create_decorator(CREATE_KEYWORDS)
+delete = create_decorator(DELETE_KEYWORDS)
+query = create_decorator(QUERY_KEYWORDS)
+update = create_decorator(UPDATE_KEYWORDS)
